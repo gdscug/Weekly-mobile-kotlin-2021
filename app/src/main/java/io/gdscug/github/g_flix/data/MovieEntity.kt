ecdad08bb25094741c248b6cba5bf27d35f4f02f
Faiz Authar <faizauthar@gmail.com>
2022-02-04 10:33:18 +0700

    Home: MovieEntity: Initial code

---
+++ app/src/main/java/io/gdscug/github/g_flix/data/MovieEntity.kt
@@ -0,0 +1,8 @@
+package io.gdscug.github.g_flix.data
+
+data class MovieEntity(
+    var movieId: String,
+    var movieTitle: String,
+    var moviePoster: String,
+    var movieDescription: String,
+)
