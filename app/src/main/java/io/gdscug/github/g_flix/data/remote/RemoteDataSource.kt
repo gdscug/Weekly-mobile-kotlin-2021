2706863ced384fa011ce794916921a2e190d781e
Faiz Authar <faizauthar@gmail.com>
2022-05-24 23:43:31 +0700

    data: remote: RemoteDataSource: getMovieRecomendation

--- app/src/main/java/io/gdscug/github/g_flix/data/remote/RemoteDataSource.kt
+++ app/src/main/java/io/gdscug/github/g_flix/data/remote/RemoteDataSource.kt
@@ -2,6 +2,7 @@ package io.gdscug.github.g_flix.data.remote
 
 import io.gdscug.github.g_flix.api.ApiConfig
 import io.gdscug.github.g_flix.data.remote.response.MovieResponse
+import io.gdscug.github.g_flix.data.remote.response.RecommendationResponse
 import retrofit2.Call
 
 class RemoteDataSource {
@@ -17,4 +18,8 @@ class RemoteDataSource {
 
     // fetch data from API for AllMovies
     fun getAllMovies(): Call<MovieResponse> = ApiConfig.getApiService().getAllMovies()
+
+    // fetch data from API for Movie Recomendation
+    fun getMovieRecomendation(id: String): Call<RecommendationResponse> =
+        ApiConfig.getApiService().getRecomendation(id)
 }
