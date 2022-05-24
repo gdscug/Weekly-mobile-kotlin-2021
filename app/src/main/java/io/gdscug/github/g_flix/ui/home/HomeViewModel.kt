f0c8f281363cbce386eec27aa4a8cb774c75cd50
Faiz Authar <faizauthar@gmail.com>
2022-05-24 23:49:39 +0700

    HomeViewModel Implement Movie Recomendation using random id between 1..600

--- app/src/main/java/io/gdscug/github/g_flix/ui/home/HomeViewModel.kt
+++ app/src/main/java/io/gdscug/github/g_flix/ui/home/HomeViewModel.kt
@@ -7,4 +7,11 @@ import io.gdscug.github.g_flix.data.local.entity.MovieEntity
 
 class HomeViewModel(private val gFlixRepository: GFlixRepository) : ViewModel() {
     fun getMovies(): LiveData<List<MovieEntity>> = gFlixRepository.getAllMovies()
+
+    fun getMovieRecomendation(): LiveData<List<MovieEntity>> =
+        gFlixRepository.getMovieRecomendation(random().toString())
+
+    fun random(): Int {
+        return (1..600).random()
+    }
 }
