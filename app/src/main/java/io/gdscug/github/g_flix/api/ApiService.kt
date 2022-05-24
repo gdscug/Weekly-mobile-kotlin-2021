ab7e377092264cb969fd2c5f8193f2d037663379
Faiz Authar <faizauthar@gmail.com>
2022-05-24 23:10:12 +0700

    api: Initial ApiService

---
+++ app/src/main/java/io/gdscug/github/g_flix/api/ApiService.kt
@@ -0,0 +1,18 @@
+package io.gdscug.github.g_flix.api
+
+import io.gdscug.github.g_flix.data.remote.response.MovieResponse
+import io.gdscug.github.g_flix.data.remote.response.RecommendationResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiService {
+
+    @GET("movies")
+    fun getAllMovies(): Call<MovieResponse>
+
+    @GET("recommendation/{id}")
+    fun getRecomendation(
+        @Path("id") id: String
+    ): Call<RecommendationResponse>
+}
