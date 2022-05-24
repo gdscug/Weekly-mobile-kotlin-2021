7f8225ccf75fc9345cd7ac0b8941a88a1f4d2605
Faiz Authar <faizauthar@gmail.com>
2022-05-24 23:46:46 +0700

    data: local: add null exception

--- app/src/main/java/io/gdscug/github/g_flix/data/local/entity/MovieEntity.kt
+++ app/src/main/java/io/gdscug/github/g_flix/data/local/entity/MovieEntity.kt
@@ -5,8 +5,8 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class MovieEntity(
-    var movieId: String,
-    var movieTitle: String,
-    var moviePoster: String,
-    var movieDescription: String,
+    var movieId: String?,
+    var movieTitle: String?,
+    var moviePoster: String?,
+    var movieDescription: String?,
 ) : Parcelable
