8019a2615f304448f3f7e3773d7f335e840d5832
Faiz Authar <faizauthar@gmail.com>
2022-05-24 21:00:18 +0700

    Home: Caraousel: Implement caraousel into HomeActivity

--- app/src/main/java/io/gdscug/github/g_flix/ui/home/HomeActivity.kt
+++ app/src/main/java/io/gdscug/github/g_flix/ui/home/HomeActivity.kt
@@ -2,8 +2,12 @@ package io.gdscug.github.g_flix.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import io.gdscug.github.g_flix.databinding.ActivityHomeBinding
 import io.gdscug.github.g_flix.databinding.ContentHomeBinding
+import io.gdscug.github.g_flix.utils.recycleview.LinearPageIndicatorDecoration
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -17,5 +21,29 @@ class HomeActivity : AppCompatActivity() {
 
         contentHomeBinding = binding.contentHome
         setSupportActionBar(binding.toolbar)
+
+        // viewModel
+        val viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        val movies = viewModel.getMovies()
+
+        // Caraousel adapter
+        val homeCaraouselAdapter = HomeCaraouselAdapter()
+
+        // Limit caraousel to 5 items
+        homeCaraouselAdapter.setMovies(movies.subList(0, 5))
+
+        with(contentHomeBinding.rvCarouselHome) {
+            // Horizontal Scroll
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+            // Snap it!
+            PagerSnapHelper().attachToRecyclerView(this)
+            setHasFixedSize(true)
+
+            // add page indicator
+            addItemDecoration(LinearPageIndicatorDecoration())
+
+            adapter = homeCaraouselAdapter
+        }
     }
 }
