8056f530ecf6cc18860a1d2e6833ca62a5b859c5
Faiz Authar <faizauthar@gmail.com>
2022-05-24 20:33:05 +0700

    HomeActivity: CoordinatorLayout[2/4]: call viewBinding for content_home

--- app/src/main/java/io/gdscug/github/g_flix/ui/home/HomeActivity.kt
+++ app/src/main/java/io/gdscug/github/g_flix/ui/home/HomeActivity.kt
@@ -3,14 +3,19 @@ package io.gdscug.github.g_flix.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import io.gdscug.github.g_flix.databinding.ActivityHomeBinding
+import io.gdscug.github.g_flix.databinding.ContentHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var contentHomeBinding: ContentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        contentHomeBinding = binding.contentHome
+        setSupportActionBar(binding.toolbar)
     }
 }
